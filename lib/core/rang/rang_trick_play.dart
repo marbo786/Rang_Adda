8558b51e7915a7309f7cd28ece8fea2451e2890a
Asad <asadakmal11@icloud.com>
2026-06-21 03:18:50 +0500

    Add RangGameState and RangTrickPlay classes to manage game state and trick plays

---
+++ lib/core/rang/rang_trick_play.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+import '../models/card_model.dart';
+
+/// Represents a single card played by one player within a Rang trick.
+/// Mirrors [TrickPlay] from [ThullaGameState] for consistency.
+class RangTrickPlay extends Equatable {
+  final String playerId;
+  final PlayingCard card;
+
+  const RangTrickPlay({required this.playerId, required this.card});
+
+  @override
+  List<Object?> get props => [playerId, card];
+
+  Map<String, dynamic> toJson() => {
+        'playerId': playerId,
+        'card': card.toJson(),
+      };
+
+  factory RangTrickPlay.fromJson(Map<String, dynamic> json) {
+    return RangTrickPlay(
+      playerId: json['playerId'] as String,
+      card: PlayingCard.fromJson(json['card'] as Map<String, dynamic>),
+    );
+  }
+}
